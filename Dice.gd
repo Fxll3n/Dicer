9af947882364e6117d16a91638211086a46e98d4
Angel Bitsov <angel.bitsov@gmail.com>
2025-05-05 00:11:51 -0500

    Hackatime functionality.

--- Dice.gd
+++ Dice.gd
@@ -1,2 +1,12 @@
 class_name Dice
-extends Node3D
+extends Node2D
+
+var sides: Array[int] = [1, 2, 3, 4, 5, 6]
+
+func _ready() -> void:
+	print("Hello World.")
+	print("Nice to meet you. I'm Angel")
+	print("I've been doing game design for 4+ years.")
+	print("ive been worried about my time management skills.")
+	
+	print(sides.pick_random())
